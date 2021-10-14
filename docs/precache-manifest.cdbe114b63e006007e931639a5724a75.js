72563ffe6e0fb7fc2820ee3c8c331765d6b66abe
Jacob Crabtree <Jacob.Crabtree@sckans.edu>
2021-10-14 10:39:17 -0500

    edit

---
+++ docs/precache-manifest.cdbe114b63e006007e931639a5724a75.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cde365f5c9712609a562f2d40d89b5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e88fdaa95a28d320e385",
+    "url": "/portfolio/static/css/main.6242967d.chunk.css"
+  },
+  {
+    "revision": "3275c2a99c5680dbdd9b",
+    "url": "/portfolio/static/js/2.5a3542f6.chunk.js"
+  },
+  {
+    "revision": "0def066106b5692cd5c135dcafc5767d",
+    "url": "/portfolio/static/js/2.5a3542f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88fdaa95a28d320e385",
+    "url": "/portfolio/static/js/main.76057655.chunk.js"
+  },
+  {
+    "revision": "62dea3017bd2ba8df9d9",
+    "url": "/portfolio/static/js/runtime-main.0a162c52.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/portfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "55489cdf0b2da2e0c4596cf8685479ff",
+    "url": "/portfolio/static/media/feature-tile-icon-01.55489cdf.svg"
+  },
+  {
+    "revision": "7cfdfd814fd517e938eebf55c4903abb",
+    "url": "/portfolio/static/media/feature-tile-icon-02.7cfdfd81.svg"
+  },
+  {
+    "revision": "2b578018bf75bee8ee8611fb0b84062f",
+    "url": "/portfolio/static/media/feature-tile-icon-03.2b578018.svg"
+  },
+  {
+    "revision": "69b381b3dce14ca2737d0e6fc0e69bef",
+    "url": "/portfolio/static/media/feature-tile-icon-04.69b381b3.svg"
+  },
+  {
+    "revision": "bcbc655cb90ceffdf40f167206b9e726",
+    "url": "/portfolio/static/media/feature-tile-icon-05.bcbc655c.svg"
+  },
+  {
+    "revision": "a658a99693e9178da569dba829d9c601",
+    "url": "/portfolio/static/media/feature-tile-icon-06.a658a996.svg"
+  },
+  {
+    "revision": "93f669e310b5601ed26970e165f627af",
+    "url": "/portfolio/static/media/features-split-image-01.93f669e3.png"
+  },
+  {
+    "revision": "18b53fc8107719b7a1b22bac0b845e77",
+    "url": "/portfolio/static/media/features-split-image-02.18b53fc8.png"
+  },
+  {
+    "revision": "b4013cf0edcd30dc3f56124735b61dba",
+    "url": "/portfolio/static/media/features-split-image-03.b4013cf0.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/portfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/portfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/portfolio/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "328246bf0091f1195efad5b0b36d76c6",
+    "url": "/portfolio/static/media/video-placeholder.328246bf.jpg"
+  }
+]);
